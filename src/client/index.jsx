93a2c0bf02eec93e3d0c87255f16126b544a6f24
Even Gabrielsen <gabrielsen.even@gmail.com>
2018-10-25 12:50:19 +0200

    Separate app into own component and simplify redux store wrapper component

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,29 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
-class App extends Component {
-  notFound() {
-    return (<div><h1>404: not found</h1></div>)
-  }
+import App from './app'
 
-  render() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={() => (<div><h1>World</h1></div>)} />
-            <Route exact path="/1" />
-            <Route exact path="/2" />
-            <Route exact path="/3" />
-            <Route component={this.notFound} />
-          </Switch>
-        </BrowserRouter>
-      </Provider>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+  <Provider store={store()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
