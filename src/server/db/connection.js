94924f365064d70cdb18e74162635a871949f0a7
Even Gabrielsen <gabrielsen.even@gmail.com>
2018-10-31 11:25:31 +0100

    Default import of object instead of package

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -1,7 +1,7 @@
-const pg = require('pg-rxjs')
+const { Pool } = require('pg-rxjs')
 
 const connector = async () => {
-  const pool = new pg.Pool('postgres://postgres:h723aHdsa73@db:5432/postgres')
+  const pool = new Pool('postgres://postgres:h723aHdsa73@db:5432/postgres')
 
   const initialize = require('./initializer')(pool)
   await initialize.tables().subscribe();
