1ced87d75f030c8596c697c2cf06104e9b17539c
Even Gabrielsen <gabrielsen.even@gmail.com>
2018-10-31 11:18:31 +0100

    Modularize react app

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,21 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './components/login'
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={() => (<div><h1>Home</h1></div>)} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" />
-          <Route exact path="/3" />
-          <Route component={() => (<div><h1>404: not found</h1></div>)} />
-        </Switch>
-      </BrowserRouter>
-    )
-  }
+export default App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={() => (<div><h1>Home</h1></div>)} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" />
+        <Route exact path="/3" />
+        <Route component={() => (<div><h1>404: not found</h1></div>)} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
-
-export default App
