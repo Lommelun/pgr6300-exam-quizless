33ef5e2b7595ec4338b64f264b077ed2dc0ffcfb
Even Gabrielsen <gabrielsen.even@gmail.com>
2018-11-18 02:35:53 +0100

    Initialize websocket event handlers with redux store

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { init } from './websocket/connection'
 
 import App from './app'
 
+const store = store()
+init(store)
+
 ReactDOM.render(
-  <Provider store={store()}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
