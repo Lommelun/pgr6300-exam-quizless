c5887eb016ab7d14aa710bea667cad669323d625
Even Gabrielsen <gabrielsen.even@gmail.com>
2018-11-18 02:37:06 +0100

    Remove unnecessary user reducer after splitting auth and registration

---
+++ src/client/actions/user.creators.js
@@ -0,0 +1,54 @@
+import { userConsts } from './action.types'
+
+function login(username, password) {
+  return dispatch => {
+    dispatch(() => { return { type: userConsts.LOGIN_REQUEST, username } });
+
+    fetch('/api/auth/authenticate/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: username, password: password })
+    }).then(JSON.parse)
+      .then(user => {
+        dispatch(() => { return { type: userConsts.LOGIN_SUCCESS, user } })
+      })
+      .catch(error => {
+        dispatch(() => { return { type: userConsts.LOGIN_FAILURE, error } })
+      })
+  }
+}
+
+function logout() {
+  dispatch(() => { return { type: userConsts.LOGOUT_REQUEST } })
+
+  fetch('/api/register/', { method: 'POST' })
+    .then(res => {
+      dispatch(() => { return { type: userConsts.LOGOUT_SUCCESS } })
+    })
+    .catch(err => {
+      dispatch(() => { return { type: userConsts.LOGOUT_FAILURE } })
+    })
+}
+
+function register(username, password) {
+  dispatch(() => {
+    return {
+      type: userConsts.REGISTER_REQUEST, user: { username: username, password: password }
+    }
+  })
+
+  fetch('/api/auth/logout/', { method: 'POST' })
+    .then(JSON.parse)
+    .then(user => {
+      dispatch(() => { return { type: userConsts.REGISTER_SUCCESS, user } })
+    })
+    .catch(err => {
+      dispatch(() => { return { type: userConsts.REGISTER_FAILURE, err } })
+    })
+}
+
+export default userActions = {
+  login,
+  logout,
+  register
+}
