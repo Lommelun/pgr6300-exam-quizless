1c8a41aa9ffca9769cc59d3aed751227226a7ad9
Even <gabrielsen.even@gmail.com>
2018-11-19 22:51:21 +0100

    Remove unused reference, reimplement later

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './components/login'
-import Home from './components/home'
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={() => <div><h1>Home page</h1></div>} />
           <Route exact path="/login" component={Login} />
           <Route component={() => (<div><h1>404: not found</h1></div>)} />
         </Switch>
