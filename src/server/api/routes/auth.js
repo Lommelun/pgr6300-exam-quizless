ca6cc9f0af4d8832eed703edd65eeb5b19ecff27
Even Gabrielsen <gabrielsen.even@gmail.com>
2018-11-19 21:28:10 +0100

    Add endpoint to fetch user after registration, possible to use redirections in the future

--- src/server/api/routes/auth.js
+++ src/server/api/routes/auth.js
@@ -42,6 +42,22 @@ router.post('/authenticate', passport.authenticate('local'), (req, res) => {
   res.status(204).send()
 })
 
+router.get('/users/:username', async (req, res) => {
+  if (!req.user) {
+    res.status(401).send()
+    return
+  }
+
+  if (req.params.username) {
+    const { id, username } = await User.findOne({ username: req.params.username })
+
+    res.status(200).send(JSON.stringify({ id, username }))
+    return
+  }
+
+  res.status(400).send()
+})
+
 router.post('/logout', (req, res) => {
   req.logout()
   res.status(204).send()
