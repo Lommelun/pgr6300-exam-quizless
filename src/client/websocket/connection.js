7cf02767022e39e5d923d7e2e4564b0eb0620463
Even Gabrielsen <gabrielsen.even@gmail.com>
2018-11-20 22:38:14 +0100

    Update websocket events

--- src/client/websocket/connection.js
+++ src/client/websocket/connection.js
@@ -1,19 +1,17 @@
 import io from 'socket.io-client';
-import { alertConsts, gameConsts, roomConsts, userConsts } from '../redux/actions/action.types';
+import { alertConsts, gameConsts, roomConsts, userConsts } from '../redux/actions/action.types'
 
 const socket = io()
 
-export const init = (store) => {
-  const types = [
-    ...Object.values(alertConsts),
-    ...Object.values(gameConsts),
-    ...Object.values(roomConsts),
-    ...Object.values(userConsts)
-  ]
+const wstypes = [
+  ...Object.values(alertConsts),
+  ...Object.values(gameConsts),
+  ...Object.values(roomConsts),
+  ...Object.values(userConsts)
+].filter(type => type.startsWith('ws/'))
 
-  types.forEach(type => socket.on(type, (payload) => {
-    store.dispatch({ type, payload })
-  }))
+export const init = store => {
+  wstypes.forEach(type => socket.on(type, payload => store.dispatch({ type, payload })))
 }
 
 export const emit = (type, payload) => socket.emit(type, payload);
